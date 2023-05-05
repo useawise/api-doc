a1c8426f5cd58bec9563e4dbc23eb9c161a54e30
ricardo.severo@quantosobra.com.br <ricardo.severo@quantosobra.com.br>
2023-05-05 14:44:51 -0300

    Adiciona descrição ao product v2

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjc3MjM/spectral.js?branch_slug=master&token=70108b00-a949-491a-a555-d11c31b80984";
+export default { extends: ruleset };
